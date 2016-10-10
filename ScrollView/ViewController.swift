be8c304afdc120ad1a5ffa5106e005f5287b37f0
Anh Lê Việt <anhleviet@anhs-mac.local>
2016-10-10 11:58:47 +0700

    commit

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imgArray = [UIImage]()
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imgArray = [#imageLiteral(resourceName: "2A-3-1"),#imageLiteral(resourceName: "image-13"),#imageLiteral(resourceName: "vjamorotezzukhdvpccc"),#imageLiteral(resourceName: "0904234")]
+        
+        scrollView.frame = view.frame
+        for i in 0..<imgArray.count{
+           let imageView = UIImageView()
+            imageView.image = imgArray[i]
+        
+            imageView.contentMode = .scaleAspectFit
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect (x: CGFloat(xPosition), y : 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+            scrollView.contentSize.width =  scrollView.frame.width * CGFloat(i + 1)
+           
+            scrollView.addSubview(imageView)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
